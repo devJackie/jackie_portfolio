c8da38013fddc2a937a55c9778efbe46a54d1ba0
devJackie <sypioneerdev@gmail.com>
2015-06-02 18:22:34 +0900

    2015.06.02

--- src/main/java/io/obreca/paradisus/service/impl/ContactServiceImpl.java
+++ src/main/java/io/obreca/paradisus/service/impl/ContactServiceImpl.java
@@ -2,7 +2,12 @@ package io.obreca.paradisus.service.impl;
 
 import io.obreca.paradisus.service.ContactService;
 import io.obreca.paradisus.domain.Contact;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
+import javax.mail.internet.MimeMessage;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -10,8 +15,27 @@ import java.util.Map;
  */
 public class ContactServiceImpl implements ContactService {
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @Override
     public Map<String, Object> sendMail(Contact contact) {
-        return null;
+
+        Map<String, Object> result = new HashMap<String, Object>();
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+            messageHelper.setTo("devjackie83@gmail.com");
+            messageHelper.setText("메일 테스트");
+            messageHelper.setFrom("sypioneerdev@gmail.com");
+            messageHelper.setSubject("test");
+
+            javaMailSender.send(message);
+            result.put("result", "success");
+        } catch (Exception e) {
+            result.put("result", "failure");
+            e.printStackTrace();
+        }
+        return result;
     }
 }
